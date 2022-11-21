acefd2c36b5bfb3a3c70f087d22d2724a1c586e8
Tom Harris <tomharris3@me.com>
2022-11-21 10:22:59 +0000

    add sphere/ball class

---
+++ 3DObjects/Ball.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Ball
+{
+    private double radius;
+    public Ball(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double Volume()
+    {
+        double volume = (4.0 / 3 * Math.PI * Math.Pow(radius, 3));
+        return Math.Round(volume, 2);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("C# Classes");
+﻿Box myBox = new Box(2, 2, 2);
+Ball myBall = new Ball(2);
+
+Console.WriteLine($"The volume of myBox is {myBox.Volume()}");
+Console.WriteLine($"The volume of myBall is {myBall.Volume()}");
