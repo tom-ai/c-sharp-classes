5876bb9800932719d2fce1239a3775ef67ab7f41
Tom Harris <tomharris3@me.com>
2022-11-21 16:26:32 +0000

    put everything into classes

--- Program.cs
+++ Program.cs
@@ -1,32 +1,41 @@
-﻿Box myBox = new Box(2, 2, 2);
-Ball myBall = new Ball(2);
+﻿MakeShapes();
+MakeStacks();
 
-List<int> widths = new List<int>() { 2, 3, 4 };
-List<int> heights = new List<int>() { 2, 3, 4 };
-List<int> depths = new List<int>() { 2, 3, 4 };
-
-List<Box> boxes = new List<Box>();
-
-for (int i = 0; i < widths.Count; i++)
+void MakeShapes()
 {
-    Box box = new Box(widths[i], heights[i], depths[i]);
-    boxes.Add(box);
+    Box myBox = new Box(2, 2, 2);
+    Ball myBall = new Ball(2);
+
+    List<int> widths = new List<int>() { 2, 3, 4 };
+    List<int> heights = new List<int>() { 2, 3, 4 };
+    List<int> depths = new List<int>() { 2, 3, 4 };
+
+    List<Box> boxes = new List<Box>();
+
+    for (int i = 0; i < widths.Count; i++)
+    {
+        Box box = new Box(widths[i], heights[i], depths[i]);
+        boxes.Add(box);
+    }
+
+    foreach (Box box in boxes)
+    {
+        Console.WriteLine(box.Volume());
+    }
+
+    Console.WriteLine($"The volume of myBox is {myBox.Volume()}");
+    Console.WriteLine($"The volume of myBall is {myBall.Volume()}");
 }
 
-foreach (Box box in boxes)
+void MakeStacks()
 {
-    Console.WriteLine(box.Volume());
-}
+    Stack myStack = new Stack();
 
-Console.WriteLine($"The volume of myBox is {myBox.Volume()}");
-Console.WriteLine($"The volume of myBall is {myBall.Volume()}");
+    for (int i = 0; i < 10; i++) myStack.Push(i);
 
-Stack myStack = new Stack();
-
-for (int i = 0; i < 10; i++) myStack.Push(i);
-
-Console.WriteLine("Each item popped off myStack: ");
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine(myStack.Pop());
+    Console.WriteLine("Each item popped off myStack: ");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(myStack.Pop());
+    }
 }
